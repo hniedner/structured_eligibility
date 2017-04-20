b105bc97281a419a9429367e39ba070309b27e64
R. Hannes Niedner <hniedner@gmail.com>
2017-04-20 11:08:52 -0400

    added description to rule object and refactored label to name

--- demo.py
+++ demo.py
@@ -1,37 +1,67 @@
 from engine import RulesEditor, RuleSet
 
-patient = {'age': 44, 'gender': 'MALE', 'disease': 'bar', 'stage': 'II', 'grade': 2, 'lab': 2500}
+patient = {'age_in_years': 44,
+           'biological_gender': 'MALE',
+           'disease_nci_thesaurus_id': 'C23456',
+           'ajcc_v7_stage': 'II',
+           'hemoglobin_grams_per_deciliter': 10,
+           'white_blood_cells_per_microliter': 2500}
 
-max_age = RulesEditor.define_max_numeric_value('age', 50)
-min_age = RulesEditor.define_min_numeric_value('age', 18)
-gender = RulesEditor.define_value_contained_in_collection('gender', ['MALE', 'FEMALE'])
+max_age = RulesEditor.define_max_numeric_value('age_in_years', 50, 'The subject must not be older than 50 years!')
+min_age = RulesEditor.define_min_numeric_value('age_in_years', 18, 'The subject must be at least 18 years old!')
+gender = RulesEditor.define_value_contained_in_collection('biological_gender', ['MALE', 'FEMALE'],
+                                                          'Both male and female subjects are eligible.')
 rs1 = RuleSet({max_age, min_age, gender}, 'rs1')
-print(rs1.conjunction())
+rs1_conjunction = rs1.conjunction(
+    'The subject must be at least 18 years old and not older than 50 years old. '
+    'Both male and female subjects are eligible.',
+    'Age Range and Gender constraints')
+print(rs1_conjunction)
 
-diseases = RulesEditor.define_value_contained_in_collection('disease', ['foo', 'bar', 'foobar'])
-stages = RulesEditor.define_value_contained_in_collection('stage', ['I', 'II', 'III'])
+diseases = RulesEditor.define_value_contained_in_collection(
+    'disease_nci_thesaurus_id', ['C12345', 'C23456', 'C76543'],
+    'The subject must have diagnosis defined by one of the following NCI Thesaurus Concept Ids: '
+    '"C12345", "C23456", "C76543"'
+                                                            )
+stages = RulesEditor.define_value_contained_in_collection(
+    'ajcc_v7_stage', ['II', 'III'],
+    'The subject have a disease stage of "II" or "III" as defined by AJCC v7!')
+
 rs2 = RuleSet({diseases, stages}, 'rs2')
-print(rs2.disjunction())
+rs2_disjunction = rs2.disjunction(
+    'The subject must have diagnosis befined by one of the following NCI Thesaurus Concept Ids: '
+    '"C12345", "C23456", "C76543" or an AJCC v7 stage of "II" or "III"!', 'Diagnosis or Stage constraints')
+print(rs2_disjunction)
 
-grade = RulesEditor.define_value_contained_in_collection('grade', [1, 2, 3])
-labv = RulesEditor.define_min_numeric_value('lab', 2000)
-rs3 = RuleSet({grade, labv}, 'rs3')
-print(rs3.conjunction())
+hb = RulesEditor.define_min_numeric_value(
+    'hemoglobin_grams_per_deciliter', 8,
+    'The subject must have a hemoglobin of no less than 8 g/dl!')
 
-rsa = RuleSet({rs1.conjunction(), rs2.disjunction(), rs3.conjunction()}, 'all rules')
-print(rsa.conjunction())
+wbc = RulesEditor.define_min_numeric_value(
+    'white_blood_cells_per_microliter', 2000,
+    'The subject must have a white blood cell count of no less than 2000 cells/ul!')
 
-RulesEditor.write_rules_yaml(rsa.conjunction(), 'bar.yaml')
-RulesEditor.write_rules_json(rsa.conjunction(), 'bar.json')
+rs3 = RuleSet({hb, wbc}, 'rs3')
+rs3_conjunction = rs3.conjunction(
+    'The subject must have a hemoglobin of no less than 8 g/dl '
+    'and a white blood cell count of no less than 2000 cells/ul!',
+    'Lab Value constraints')
+print(rs3_conjunction)
 
-rule_orig = rsa.conjunction()
+rsa = RuleSet({rs1_conjunction, rs2_disjunction, rs3_conjunction}, 'all rules')
+rsa_conjunction = rsa.conjunction('Combined eligibility criteria.', 'Eligibility Criteria')
+print(rsa_conjunction)
+
+RulesEditor.write_rules_yaml(rsa_conjunction, 'bar.yaml')
+RulesEditor.write_rules_json(rsa_conjunction, 'bar.json')
+
 rule_yaml_recon = RulesEditor.read_rules_yaml('bar.yaml')
 rule_json_recon = RulesEditor.read_rules_json('bar.json')
 
-print(RulesEditor.evaluate(rule_orig.definition, patient))
+print(RulesEditor.evaluate(rsa_conjunction.definition, patient))
 print(RulesEditor.evaluate(rule_yaml_recon.definition, patient))
 print(RulesEditor.evaluate(rule_json_recon.definition, patient))
 
-print(rule_orig.definition)
+print(rsa_conjunction.definition)
 print(rule_yaml_recon.definition)
 print(rule_json_recon.definition)
